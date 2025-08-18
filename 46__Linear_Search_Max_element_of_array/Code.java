b7405d54ee1f4efbd6dcce21929bbe14af41c338
RahulXeel <rahulxeel@gmail.com>
2025-08-18 13:53:39 +0530

    Just Do It

--- 46__Linear_Search_Max_element_of_array/Code.java
+++ 46__Linear_Search_Max_element_of_array/Code.java
@@ -28,7 +28,7 @@ public class Code {
 
     public static void MAX_ELEMENT(int arr[]) {
 
-        int max = 0;
+        int max = arr[0];
 
         for (int i = 0; i < arr.length; i++) {
            
