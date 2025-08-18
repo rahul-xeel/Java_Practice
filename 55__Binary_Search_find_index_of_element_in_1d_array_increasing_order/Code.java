6279b5c338898b363497119e844596889317ea7b
RahulXeel <rahulxeel@gmail.com>
2025-08-19 00:50:38 +0530

    Just Do It

---
+++ 55__Binary_Search_find_index_of_element_in_1d_array_increasing_order/Code.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Code {
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println();
+        System.out.println("-------- BINARY SEARCH --------");
+        System.out.println();
+        System.out.println();
+        System.out.println("---- FIND THE INDEX OF ELEMENT IN 1D ARRAY-----");
+        Scanner listen = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Your current array is { 1, 2, 3, 4, 5 }");
+        System.out.println();
+        int target = listen.nextInt();
+        int arr[] = { 1, 2, 3, 4, 5 };
+        System.out.println();
+
+        FIND_INDEX_OF_ELEMENT_1D_ARRAY(arr, target);
+
+    }
+
+    //// FINDING INDEX OF ELEMENT IN 1D ARRAY
+
+    public static void FIND_INDEX_OF_ELEMENT_1D_ARRAY(int arr[], int target) {
+
+        int ans_index = 0;
+        boolean found = false;
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while (start <= end) {
+
+             mid = start + (end - start)/2;
+
+
+            if (arr[mid] == target) {
+                ans_index = mid;
+                found = true;
+                break;
+            }
+
+             else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            
+            else {
+                end = mid - 1;
+                 }
+
+           
+
+        }
+
+        if (found == true) {
+
+            System.out.println("Your index is :- " + ans_index);
+
+        } else {
+            System.out.println("Element not found");
+
+        }
+
+    }
+
+}
